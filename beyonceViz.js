3db086860851a70cf3ba7850eca7e83fa6221fe2
bellmast <jbm444@gmail.com>
2014-04-30 22:10:37 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -13,9 +13,24 @@ function runProgram() {
 }  
 
 function drawCanvas() {
+  
+  var c = document.getElementById("myCanvas");
+  var ctx=c.getContext("2d");
+  ctx.drawImage(img,0,0);
+  var imgData=ctx.getImageData(0,0,c.width,c.height);
+  // invert colors
+  for (var i=0;i<imgData.data.length;i+=4)
+    {
+    imgData.data[i]=255-imgData.data[i];
+    imgData.data[i+1]=255-imgData.data[i+1];
+    imgData.data[i+2]=255-imgData.data[i+2];
+    imgData.data[i+3]=255;
+    }
+  ctx.putImageData(imgData,0,0);
+
     for (i = 0; i < canvasWidth; i++) {
     	for (h = 0; h < canvasHeight; h++) {
-    		paper.circle(i, h, 1).attr({"stroke":"red"})
+    		
     	}
     }
 }
