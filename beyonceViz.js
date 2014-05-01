aa33cd77607a3d34aa53064d802f9d2bd2d93621
bellmast <jbm444@gmail.com>
2014-04-30 21:37:21 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -8,13 +8,13 @@ function runProgram() {
     img = document.getElementById('originalImage')
     canvasWidth = img.clientWidth;
     canvasHeight = img.clientHeight;
-    paper = new Raphael(document.getElementById('canvas_container'), canvasWidth, canvasHeight);  
+    paper = new Raphael(canvasWidth, canvasHeight, canvasWidth, canvasHeight);  
     {drawCanvas()};       
 }  
 
 function drawCanvas() {
-    for (i = 0; i < canvasWidth; i++) {
-    	for (h = 0; h < canvasHeight; h++) {
+    for (i = canvasWidth; i < canvasWidth*2; i++) {
+    	for (h = canvasHeight; h < canvasHeight*2; h++) {
     		paper.circle(i, h, 1).attr("stroke"="red")
     	}
     }
