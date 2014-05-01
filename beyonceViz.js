31d74450fb8582f51c79b6bea6bbb675f75a327d
bellmast <jbm444@gmail.com>
2014-04-30 23:16:45 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -7,8 +7,22 @@ $(document).ready(function () {runProgram()});
 function runProgram() {
     img = document.getElementById('originalImage')
     c = document.getElementById("myCanvas");
+    
     canvasWidth = img.clientWidth;
     canvasHeight = img.clientHeight;
+    var ctx=c.getContext("2d");
+
+    var imgData=ctx.getImageData(0,0,canvasWidth,canvasHeight);
+      // invert colors
+    for (var i=0;i<imgData.data.length;i+=4)
+        {
+        imgData.data[i]=255-imgData.data[i];
+        imgData.data[i+1]=255-imgData.data[i+1];
+        imgData.data[i+2]=255-imgData.data[i+2];
+        imgData.data[i+3]=255;
+        }
+    ctx.putImageData(imgData,0,0);
+
     paper = new Raphael(canvasWidth, 0, canvasWidth, canvasHeight);  
     {drawCanvas()};       
 }  
@@ -16,18 +30,6 @@ function runProgram() {
 function drawCanvas() {
   
 
-  var ctx=c.getContext("2d");
-
-  var imgData=ctx.getImageData(0,0,canvasWidth,canvasHeight);
-  // invert colors
-  for (var i=0;i<imgData.data.length;i+=4)
-    {
-    imgData.data[i]=255-imgData.data[i];
-    imgData.data[i+1]=255-imgData.data[i+1];
-    imgData.data[i+2]=255-imgData.data[i+2];
-    imgData.data[i+3]=255;
-    }
-  ctx.putImageData(imgData,0,0);
 
     for (i = 0; i < canvasWidth; i++) {
     	for (h = 0; h < canvasHeight; h++) {
