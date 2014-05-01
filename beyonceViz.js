e6be5001763316f11a86acf7bebd9159d368efb5
bellmast <jbm444@gmail.com>
2014-04-30 21:04:04 -0500

    Create beyonceViz.js

---
+++ beyonceViz.js
@@ -0,0 +1,15 @@
+var paper;
+var canvasWidth = 800;
+var canvasHeight = 700;
+var padding = 10;
+
+$(document).ready(function () {runProgram()});
+
+function runProgram() {
+    paper = new Raphael(document.getElementById('canvas_container'), canvasWidth, canvasHeight);  
+    {drawCanvas()});       
+}  
+
+function drawCanvas() {
+    paper.circle(150, 150, 10)
+}
