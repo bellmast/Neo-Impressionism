4b152bfa6d6e0313f664b4270ac6210620fb748c
bellmast <jbm444@gmail.com>
2014-05-04 18:07:19 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -5,13 +5,18 @@ var padding = 10;
 $(document).ready(function () {runProgram()});
 
 function runProgram() {
-    img = document.getElementById('originalImage')
-    c = document.getElementById("myCanvas");
+    document.getelementById("originalImage").onload = function() {
+        canvas = document.getElementById("myCanvas");
+        context = canvas.getContext("2d");
+        img = document.getElementById('originalImage');
+        context.drawImage(img, 0, 0)
+    }
+    
+    
     
     canvasWidth = 600;
     canvasHeight = 600;
-    ctx=c.getContext("2d");
-    ctx.drawImage(img, 0, 0)
+    
 
     
 
