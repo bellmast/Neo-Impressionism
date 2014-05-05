04bb5fe5310b25c19607dcba4b5bbf0c125e60e3
bellmast <jbm444@gmail.com>
2014-05-04 20:21:36 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -32,10 +32,10 @@ function drawCanvas() {
     widthMod = 12
     widthTracker = 0
     heightTracker = 0
-    for (h = 0; h < canvasHeight; h+=heightMod) {
-    	for (i = 0; i < canvasWidth; i+=widthMod) {
+    for (h = 0; h < canvasHeight; h+=6) {
+    	for (i = 0; i < canvasWidth; i+=12) {
             
-            imgData=context.getImageData(widthTracker, heightTracker, widthMod, heightMod);
+            imgData=context.getImageData(widthTracker, heightTracker, 12, 6);
 
             redCount = 0
             greenCount = 0
@@ -54,9 +54,13 @@ function drawCanvas() {
             avgGreen = greenCount/count
             avgBlue = blueCount/count
             avgAlpha = alphaCount/count
-            filler = "rgba("+avgRed+", "+avgGreen+", "+avgBlue+", "+avgAlpha+")"
-            brushSet.push(paper.rect(i, h, widthMod, heightMod).attr({fill:filler, stroke:"none"}))
-
+            fillerRed = "rgba("+avgRed+", "+0+", "+0+", "+avgAlpha+")"
+            fillerGreen = "rgba("+0+", "+avgGreen+", "+0+", "+avgAlpha+")"
+            fillerBlue = "rgba("+0+", "+0+", "+avgBlue+", "+avgAlpha+")"
+            //brushSet.push(paper.rect(i, h, widthMod, heightMod).attr({fill:filler, stroke:"none"}))
+            brushSet.push(paper.circle(i-4, h, 2)).attr({fill:fillerRed, stroke:"none"})
+            brushSet.push(paper.circle(i, h, 2)).attr({fill:fillerGreen, stroke:"none"})
+            brushSet.push(paper.circle(i+4, h, 2)).attr({fill:fillerBlue, stroke:"none"})
             //
             widthTracker += widthMod
     		widthMod = Math.floor(Math.random()*4)+2
