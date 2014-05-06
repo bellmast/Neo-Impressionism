d3eb546e45e5833881246caa7a223e21c48776a1
bellmast <jbm444@gmail.com>
2014-05-05 21:02:22 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -54,9 +54,9 @@ function drawCanvas() {
             avgGreen = greenCount/count
             avgBlue = blueCount/count
             avgAlpha = alphaCount/count
-            hsl = rgbToHsv(avgRed, avgGreen, avgBlue)
 
-            filler = "hsl("+avgRed+", "+avgGreen+", "+avgBlue+")"
+
+            filler = "rgb("+avgRed+", "+avgGreen+", "+avgBlue+")"
             fillerRed = "rgba("+avgRed+", "+0+", "+0+", "+avgAlpha+")"
             fillerGreen = "rgba("+0+", "+avgGreen+", "+0+", "+avgAlpha+")"
             fillerBlue = "rgba("+0+", "+0+", "+avgBlue+", "+avgAlpha+")"
@@ -76,30 +76,3 @@ function drawCanvas() {
     }
 }
 
-function rgbToHsv(r, g, b) {
-    var
-        min = Math.min(r, g, b),
-        max = Math.max(r, g, b),
-        delta = max - min,
-        h, s, v = max;
-
-    v = Math.floor(max / 255 * 100);
-    if ( max != 0 )
-        s = Math.floor(delta / max * 100);
-    else {
-        // black
-        return [0, 0, 0];
-    }
-
-    if( r == max )
-        h = ( g - b ) / delta;         // between yellow & magenta
-    else if( g == max )
-        h = 2 + ( b - r ) / delta;     // between cyan & yellow
-    else
-        h = 4 + ( r - g ) / delta;     // between magenta & cyan
-
-    h = Math.floor(h * 60);            // degrees
-    if( h < 0 ) h += 360;
-
-    return [h, s, v];
-}
