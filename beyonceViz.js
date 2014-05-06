819f048c6f22d13def46c98d7d240a97aa0b1e75
bellmast <jbm444@gmail.com>
2014-05-05 21:16:41 -0500

    Update beyonceViz.js

--- beyonceViz.js
+++ beyonceViz.js
@@ -55,8 +55,8 @@ function drawCanvas() {
             avgBlue = blueCount/count
             avgAlpha = alphaCount/count
 
-
-            filler = "rgb("+avgRed+", "+avgGreen+", "+avgBlue+")"
+            hsl = rgbToHsl(avgRed, avgGreen, avgBlue)
+            filler = "hsl("+hsl[0]+", "+hsl[1]+"%, "+hsl[2]+"%)"
             fillerRed = "rgba("+avgRed+", "+0+", "+0+", "+avgAlpha+")"
             fillerGreen = "rgba("+0+", "+avgGreen+", "+0+", "+avgAlpha+")"
             fillerBlue = "rgba("+0+", "+0+", "+avgBlue+", "+avgAlpha+")"
@@ -74,5 +74,26 @@ function drawCanvas() {
     heightTracker += 12
     heightMod = Math.floor(Math.random()*8)+4
     }
+}
+
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
 }
 
